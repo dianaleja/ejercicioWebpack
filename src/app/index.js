9ca38962e9bc62d9cfe82b89411b7fae74a0a8fe
diana pinzon <diaphi@hotmail.com>
2023-04-27 18:34:25 -0500

    creación de estilos sass, cambios nombre clases con metodo bem, funciones js para ventanas emergentes, promeros pasos para el diseño responsive designer

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,16 @@
 import "./index.js"
 import "./styles/styles.css"
+const featuresLink = document.getElementById("features-link");
+const companyLink = document.getElementById("company-link");
+
+featuresLink.addEventListener("click", () => {
+  console.log("Hiciste clic en el enlace de Features");
+  const featuresDropdown = document.querySelector(".features-dropdown");
+  featuresDropdown.classList.toggle("show-dropdown");
+});
+
+companyLink.addEventListener("click", () => {
+  console.log("Hiciste clic en el enlace de Company");
+  const companyDropdown = document.querySelector(".company-dropdown");
+  companyDropdown.classList.toggle("company-dropdown--show");
+});
