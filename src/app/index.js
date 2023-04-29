02ed88b2295a3729549af5321a29b1f5c3b8e620
diana pinzon <diaphi@hotmail.com>
2023-04-28 23:29:44 -0500

    cambios js

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,3 @@
-const ham = document.querySelector('.ham')
-     
-       ham.addEventListener('click', e=>{
-         document.querySelector('.mobile__nav').classList.add('show')
-         document.querySelector('.overlay').style.display = 'block'
-         document.querySelector('body').classList.add('lock')
-       })
-       document.querySelector('#close-Menu').addEventListener('click', e =>{
-         document.querySelector('.mobile__nav').classList.remove('show')
-         document.querySelector('.overlay').style.display = 'none'
-         document.querySelector('body').classList.remove('lock')
-       })
+import '../index.html';
+// Importar archivo scss que genera sass
+import './styles/styles.css';
